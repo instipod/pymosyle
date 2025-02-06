d757bef9884a802151d572070835b251e3711506
instipod <instipod@instipod.com>
2025-02-06 17:59:20 -0600

    add license header

--- pymosyle.py
+++ pymosyle.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) Michael Kelly. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
 import json
 import time
 import requests
